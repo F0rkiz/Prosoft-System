da8448ca70abe2fcfef01b2b182bfb2dcc05c545
F0rkiz <dimasharov007@gmail.com>
2026-09-19 15:24:45 +0300

    Add files via upload

---
+++ 51.cpp
@@ -0,0 +1,40 @@
+﻿class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        // Это была 1 задачка с этого кода я не кайфую
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+        backtrack(ans, board, 0, n);
+        return ans;
+    }
+
+    void backtrack(vector<vector<string>>& ans, vector<string>& board, int i, int n) {
+        if (i == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (check(board, i, j, n)) {
+                board[i][j] = 'Q';
+                backtrack(ans, board, i + 1, n);
+                board[i][j] = '.';
+            }
+
+        }
+    }
+
+    bool check(vector<string>& board, int row, int col, int n) {
+        for (int j = 0; j < n; j++) {
+            if (board[j][col] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+};
